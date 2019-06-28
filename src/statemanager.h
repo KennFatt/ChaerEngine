23dd2203e5f3e74ed5647ef5499dcf67aefcf0db
Kennan Fattahillah <clashofklen@gmail.com>
2019-06-28 15:23:38 +0700

    statemanager.h: Implement base structs and typedef.

--- src/statemanager.h
+++ src/statemanager.h
@@ -1,4 +1,31 @@
 #ifndef CE_STATEMANAGER_H
 #define CE_STATEMANAGER_H
 
+/**
+ * State's callback.
+ */
+typedef unsigned int (*i_callback)();
+typedef unsigned int (*fl_callback)(float);
+
+/**
+ * State struct.
+ */
+typedef struct
+{
+    int onInit;
+    int onDestroy;
+    float onUpdate;
+    float onDraw;
+} State;
+
+/**
+ * StageManager struct.
+ */
+typedef struct
+{
+    State **stack;
+    int capacity;
+    int currentStack;
+} StageManager;
+
 #endif
