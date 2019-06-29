KennFatt/ChaerEngine

commit 119ef35ac7d6b1b494cf199b4d9cd608895fbfb0

chaerengine.c: just make sure it passing for build_id.

chaerengine.c
@@ -5,7 +5,7 @@
 int main(int argc, char **argv)
 {
     int app_st = 0;
-    char *build_id = argc > 1 ? argv[1] : "NaN";
+    char *build_id = argc == 1 ? argv[1] : "NaN";
 
     // run the unit test.
     unsigned int test_res = ce_unittest_run();

commit 1cb97127f75f9f0630606b38184cebcad841cc21

Fixing the bug that introduced in 119ef35ac7d6b1b494cf199b4d9cd608895fbfb0

chaerengine.c
@@ -5,7 +5,7 @@
 int main(int argc, char **argv)
 {
     int app_st = 0;
-    char *build_id = argc == 1 ? argv[1] : "NaN";
+    char *build_id = argc == 2 ? argv[1] : "NaN";
 
     // run the unit test.
     unsigned int test_res = ce_unittest_run();