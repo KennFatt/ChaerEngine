67ef22cd5472959ccd3ab884bfed1473b706b79a
Kennan Fattahillah <clashofklen@gmail.com>
2019-07-02 10:50:43 +0700

    Remove unused headers from chaerengine.c and refactor unit testing.

--- chaerengine.c
+++ chaerengine.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "src/statemanager.h"
 #include "unittest.h"
 
 int main(int argc, char **argv)
@@ -8,7 +7,7 @@ int main(int argc, char **argv)
     char *build_id = argc == 2 ? argv[1] : "NaN";
 
     // run the unit test.
-    unsigned int test_res = ce_unittest_run();
+    unsigned int test_res = ce_ut_simulate();
     if (test_res > 0) {
         app_st = (int) test_res;
     }
